903d1c36ad7454c97591a3b0924a1339b5fb3942
vic_lnh <vic_leh@meten.com>
2018-09-05 15:47:43 +0800

    添加发布

---
+++ SmallReport/SmallReport.Service/QueryExceptionService.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using YiTu.DBUtility;
+
+namespace SmallReport.Service
+{
+    public static class QueryExceptionService
+    {
+        private static string ConnectionString => ConfigurationManager.ConnectionStrings["Lks"].ConnectionString;
+
+        public static bool CheckReqSync()
+        {
+            const string sql = @"SELECT a.* FROM [Lks].[stu].[StudentRequirement] a 
+                        LEFT JOIN [LksForICAS].[dbo].[StuReq] b  
+                        ON  a.StudentId=b.StudentId AND a.BeginTime=b.BeginTime
+                        WHERE a.CancelStatusType=101 AND a.RequirementStatusType= 102 
+                        AND a.BeginTime > GETDATE() 
+                        AND b.Id IS NULL 
+                        ORDER BY a.BeginTime DESC";
+            using (var reader = SqlHelper.ExecuteReader(ConnectionString, CommandType.Text, sql, new List<SqlParameter>()))
+            {
+                if (reader.Read())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static bool CheckMatchIdNull()
+        {
+            const string sql = @"SELECT * FROM LksForICAS.dbo.StuReq WHERE 1=1 
+                        AND MathingId IS NULL
+                        ORDER BY BeginTime DESC";
+            using (var reader = SqlHelper.ExecuteReader(ConnectionString, CommandType.Text, sql, new List<SqlParameter>()))
+            {
+                if (reader.Read())
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
---
+++ SmallReport/SmallReport/Jobs/QueryExceptions.cs
@@ -0,0 +1,47 @@
+﻿using Quartz;
+using SmallReport.Assist;
+using SmallReport.Assist.Quartz;
+using SmallReport.Service;
+using System;
+
+namespace SmallReport.Jobs
+{
+    public class QueryExceptions : IJob
+    {
+        #region Attr
+        private static readonly object LockObj = new object();
+
+        #endregion
+        public void Execute(IJobExecutionContext context)
+        {
+            JobHelper.Invoke(context, () =>
+            {
+                using (var lockHelper = new LockHelper(LockObj))
+                {
+                    if (lockHelper.IsTimeout) return;
+                    var result = QueryException();
+                }
+            });
+        }
+
+        private static bool QueryException()
+        {
+            var flag = true;
+            try
+            {
+                var hasValue = QueryExceptionService.CheckMatchIdNull();
+                if (hasValue)
+                {
+                    LogHelper.Error("返现异常数据，开始调用消息推送");
+                }
+            }
+            catch (Exception e)
+            {
+                LogHelper.Error("Task Exception", e);
+                flag = false;
+            }
+
+            return flag;
+        }
+    }
+}
